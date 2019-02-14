e1148ffaedfd4b9030d8513d1b663350ea5a6a9b
daiyunchao <312931671@qq.com>
2019-02-14 15:20:45 +0800

    实现了自己的dropWith方法

--- dropWhile.js
+++ dropWhile.js
@@ -21,6 +21,40 @@ import baseWhile from './.internal/baseWhile.js'
  * dropWhile(users, ({ active }) => active)
  * // => objects for ['pebbles']
  */
+
+ //我的版本
+function my_dropWhile(source_arr,dropFilter){
+	if(!Array.isArray(source_arr))throw new Error("first argument not array");
+	let slice_arr=[];
+	let index=0;
+	let ret_arr=[];
+	while(source_arr.length>slice_arr.length){
+		slice_arr[index]=source_arr[index];
+		index++;
+	}
+	if(typeof dropFilter==="function"){
+	//如果是函数
+		ret_arr=slice_arr.filter((item)=>{
+			return !dropFilter(item);
+		})
+	}
+	
+	else if (Array.isArray(dropFilter)){
+	}
+	
+	else if(typeof dropFilter==="object"){
+	ret_arr=slice_arr.filter((item)=>{
+			if(typeof item=="object"){
+				let itemStr=JSON.stringify(item);
+				let dropFilterStr=JSON.stringify(dropFilter);
+				return itemStr!==dropFilterStr;
+			}
+			return true;
+		})
+	}
+	return ret_arr;
+}
+
 function dropWhile(array, predicate) {
   return (array != null && array.length)
     ? baseWhile(array, predicate, true)
